096ecb7ed2e5de3ecddd99f33b2ccb75bf3748f3
Wung Brandon <wungbrandon27@gmail.com>
2024-08-22 23:02:43 +0100

    practise flexbox with react native

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.box1}>
+        <Text>1</Text>
+      </View>
+
+      <View style={styles.box2}>
+        <Text>2</Text>
+      </View>
+
+      <View style={styles.box3}>
+        <Text>3</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+      padding: 50,
+      flexDirection: 'row',
+      width:'80%',
+      height: 300,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    box1: {
+      backgroundColor: 'red',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    box2: {
+      backgroundColor: 'green',
+      flex: 2,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    box3: {
+      backgroundColor: 'purple',
+      flex: 3,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+
+
 });
